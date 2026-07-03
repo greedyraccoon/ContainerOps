73321ffcd746eca1e1d3c11a5856942adebd86d6
SahilDange14 <sahildange1410@gmail.com>
2026-07-03 18:20:47 +0530

    feat(expense): add expense repository

---
+++ Backend/src/main/java/com/containerops/expense/repository/ExpenseRepository.java
@@ -0,0 +1,12 @@
+package com.containerops.expense.repository;
+
+import com.containerops.expense.entity.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List<Expense> findByTripId(Long tripId);
+}
