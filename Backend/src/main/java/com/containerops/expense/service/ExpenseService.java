15914a97d950fec5d9a52fe064d32434023bb29e
SahilDange14 <sahildange1410@gmail.com>
2026-07-03 19:25:20 +0530

    feat(expense): implement expense service

---
+++ Backend/src/main/java/com/containerops/expense/service/ExpenseService.java
@@ -0,0 +1,17 @@
+package com.containerops.expense.service;
+
+import com.containerops.expense.dto.ExpenseRequestDto;
+import com.containerops.expense.dto.ExpenseResponseDto;
+import com.containerops.expense.enums.ExpenseStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface ExpenseService {
+    ExpenseResponseDto logExpense(ExpenseRequestDto requestDto);
+    ExpenseResponseDto getExpenseById(Long id);
+    List<ExpenseResponseDto> getExpensesByTripId(Long tripId);
+    ExpenseResponseDto updateExpenseDetails(Long id, ExpenseRequestDto requestDto);
+    ExpenseResponseDto updateExpenseStatus(Long id, ExpenseStatus status);
+}
